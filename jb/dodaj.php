8f134a8a8b73c7f9efb6112951986622f59e8dfe
Kebabsones <82664323+Kebabsones@users.noreply.github.com>
2022-09-07 09:19:15 +0200

    Add files via upload

--- jb/dodaj.php
+++ jb/dodaj.php
@@ -9,22 +9,9 @@
 	$rok = $_POST["rok"];
 	$oce = $_POST["ocena"];
 	
-	$sql = 'INSERT INTO filmy(gatunki_id, tytul, rok, ocena) VALUES($_POST["gatunek"], $_POST["tytul"], $_POST["rok"], $_POST["ocena"])';
-	
-	$sql1 = 'INSERT INTO filmy(gatunki_id, tytul, rok, ocena) VALUES(1, 1, 1, 1)';
-	
-	$sql1 = "INSERT INTO filmy(gatunki_id, tytul, rok, ocena) VALUES($gat, \"$tyt\", $rok, $oce)";
-	
-	echo $_POST["gatunek"];
-	echo "<br>";
-	echo $_POST["tytul"];
-	echo "<br>";
-	echo $_POST["rok"];
-	echo "<br>";
-	echo $_POST["ocena"];
-	echo "<br>";
-	
-	if(mysqli_query($conn, $sql1)) {
+	$sql = "INSERT INTO filmy(gatunki_id, tytul, rok, ocena) VALUES($gat, \"$tyt\", $rok, $oce)";
+
+	if(mysqli_query($conn, $sql)) {
 		echo "Film ".$_POST['tytul']." został dodany do bazy";
 	} else {
 		echo "error error haslo nieprawidlowe";
